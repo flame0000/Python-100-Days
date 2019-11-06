735168a1238fcdd8aea9fe4ab9457f8b067e32de
flame00000 <chentianyuan0@hotmail.com>
2019-11-06 23:49:00 +0100

    [D6] Assignment

--- Test/Palindrome_prime.py
+++ Test/Palindrome_prime.py
@@ -1,7 +1,15 @@
+'''
+from Prime_num_F import pri
+from Palindrome_number_judgement_F import Palindrome
+
+if __name__ == '__main__':
+    num = int(input('x = '))
+    print(Palindrome(num) == True and pri(num) == True)
+'''
 
 import Prime_num_F
 import Palindrome_number_judgement_F
 
 if __name__ == '__main__':
     num = int(input('x = '))
-    Print(Palindrome(num) == True and pri(num) == 0)
+    print(Palindrome_number_judgement_F.Palindrome(num) == True and Prime_num_F.pri(num) == True)
